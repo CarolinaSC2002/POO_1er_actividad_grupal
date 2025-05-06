f92c1847fd160f1bd0e4801fb7f06912d2573b0b
leidyvillacob <69911474+leidyvillacob@users.noreply.github.com>
2025-05-06 13:02:20 -0500

    Update ejercicio_1.py

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,21 +1,22 @@
 class Persona:
-    def __init__(self, nombre, apellido, documento, anio_nacimiento):
+    def __init__(self, nombre, apellidos, númeroDocumentoIdentidad, añoNacimiento):
         self.nombre = nombre
-        self.apellido = apellido
-        self.documento = documento
-        self.anio_nacimiento = anio_nacimiento
+        self.apellidos = apellidos
+        self.númeroDocumentoIdentidad = númeroDocumentoIdentidad
+        self.añoNacimiento = añoNacimiento
 
-    def imprimir_datos(self):
+    def imprimir(self):
         print("Nombre:", self.nombre)
-        print("Apellido:", self.apellido)
-        print("Documento de identidad:", self.documento)
-        print("Año de nacimiento:", self.anio_nacimiento)
+        print("Apellidos:", self.apellidos)
+        print("Número de identidad:", self.númeroDocumentoIdentidad)
+        print("Año de nacimiento:", self.añoNacimiento)
         print()
 
 # Método principal (main)
 if __name__ == "__main__":
-    persona1 = Persona("Ana", "Torres", "12345678", 1990)
-    persona2 = Persona("Luis", "Martínez", "87654321", 1985)
+    persona1 = Persona("Pedro", "Pérez", "1053121010", 1998)
+    persona2 = Persona("Luis", "León", "1053223344", 2001)
 
-    persona1.imprimir_datos()
-    persona2.imprimir_datos()
+    persona1.imprimir()
+    persona2.imprimir()
+
